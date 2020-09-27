9c1e530059c274f154ddc31b0daf2fb838b45387
rumba80 <68961064+rumba80@users.noreply.github.com>
2020-09-27 20:47:57 +0300

    Unit2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import HeroBlock from "./components/HeroBlock/HeroBlock";
+import Facts from "./components/Facts/Facts";
+import gsLogo from "./assets/GC_logo.svg";
+import ITNLogo from "./assets/ITN_logo.svg";
+import shidLogo from "./assets/shid_emozhl_logo.svg";
 
 function App() {
+  const partner = [shidLogo, ITNLogo, gsLogo];
+  const navLinks = [
+    {
+      title: "Основные факты",
+      href: "#facts",
+    },
+    {
+      title: "Индекс качества воздуха",
+      href: "#quality",
+    },
+    {
+      title: "Политика по уменьшению загрязнений",
+      href: "#politics",
+    },
+  ];
+  const factsSlides = [
+    {
+      id: "1332",
+      imgUrl: require("./assets/facts-slider/slide-1.jpg"),
+      desc: "Женщины и дети – главные жертвы загрязнения воздуха",
+    },
+    {
+      id: "1333",
+      imgUrl: require("./assets/facts-slider/slide-2.jpg"),
+      desc: "Защитники окружающей среды подвергаются преследованиям",
+    },
+    {
+      id: "1334",
+      imgUrl: require("./assets/facts-slider/slide-3.jpg"),
+      desc: "Загрязнение воздуха – нарушение прав человека",
+    },
+    {
+      id: "1335",
+      imgUrl: require("./assets/facts-slider/slide-4.jpg"),
+      desc:
+        "4,2 миллиона случаев преждевременной смерти людей из-за загрязнения атмосферного воздуха",
+    },
+    {
+      id: "1336",
+      imgUrl: require("./assets/facts-slider/slide-5.jpg"),
+      desc:
+        "Согласно оценкам, суммарный объем затрат в связи с загрязнением воздуха превышает 5 трлн долл. в год.",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeroBlock
+        title="Качество атмосферного воздуха и здоровье"
+        logos={partner}
+        links={navLinks}
+      />
+      <Facts title="Основные факты" slides={factsSlides} />
+    </>
   );
 }
 
